d4c9fd5cb79819af78647f688a6c726fcc53f9a3
PhatJack <tienphat.ng693@gmail.com>
2025-06-26 09:26:49 +0700

    fix eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -6,11 +6,29 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const compat = new FlatCompat({
-  baseDirectory: __dirname,
+	baseDirectory: __dirname,
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+	...compat.extends("next/core-web-vitals", "next/typescript"),
+	...compat.config({
+		rules: {
+			"no-unused-vars": "off",
+			"@typescript-eslint/no-unused-vars": [
+				"warn",
+			],
+			// "@next/next/no-img-element": "off",
+			// "@next/next/no-html-link-for-pages": "off",
+		},
+	}),
+	// {
+	// 	files: ["**/*.ts", "**/*.tsx"],
+	// 	languageOptions: {
+	// 		parserOptions: {
+	// 			projectService: true,
+	// 		},
+	// 	},
+	// },
 ];
 
 export default eslintConfig;
