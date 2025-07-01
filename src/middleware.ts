a40e285ee85294dd109e9b315e8fbb3f037080a1
PhatJack <tienphat.ng693@gmail.com>
2025-07-01 09:04:03 +0700

    remove swagger and add toast

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,29 +4,20 @@ import type { NextRequest } from "next/server";
 
 // This function can be marked `async` if using `await` inside
 export function middleware(request: NextRequest) {
-  return NextResponse.next();
-  // const protectedRoutes = [
-  //   "/dashboard",
-  //   "/profile",
-  //   "/solo",
-  //   "/study-goal",
-  //   "/leaderboard",
-  //   "/room",
-  //   "/room/:roomCode",
-  // ];
+  const protectedRoutes = ["/"];
 
-  // const currentPath = request.nextUrl.pathname;
-  // const isProtected = protectedRoutes.some(
-  //   (route) => currentPath === route || currentPath.startsWith(`${route}/`)
-  // );
-  // if (isProtected) {
-  //   // const token = await cookies().get("token")?.value;
-  //   if (true) {
-  //     return NextResponse.redirect(
-  //       new URL("/login", request.nextUrl.origin).toString()
-  //     );
-  //   }
-  // }
+  const currentPath = request.nextUrl.pathname;
+  const isProtected = protectedRoutes.some(
+    (route) => currentPath === route || currentPath.startsWith(`${route}/`)
+  );
+  if (isProtected) {
+    // const token = await cookies().get("token")?.value;
+    if (true) {
+      return NextResponse.redirect(
+        new URL("/login", request.nextUrl.origin).toString()
+      );
+    }
+  }
 }
 
 // See "Matching Paths" below to learn more
